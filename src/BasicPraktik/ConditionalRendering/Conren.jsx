036fad79a51f05c85de7e1daeca5fa365c5af03d
KingWahid <wahidann765@gmail.com>
2024-05-20 13:10:01 +0700

    learning more conditional rendering

--- src/BasicPraktik/ConditionalRendering/Conren.jsx
+++ src/BasicPraktik/ConditionalRendering/Conren.jsx
@@ -6,6 +6,31 @@ export default function Conren() {
   return (
     <div>
         {messages.length > 0 && <h1>You have {messages.length} unread messages</h1>}
+        {messages.length === 0 && <h1>You're all caught up!</h1>}
+
+        {messages.length > 0 && <h1>You have {messages.length} unread</h1>}
+
+        {<h1>{messages.length === 0 ? "You're all caught up!":`You have ${messages.length} unread`}</h1>}
+
+        {/* Another way*/}
+        {
+            messages.length === 0 ? 
+                <h1>You're all caught up!</h1> : 
+                <h1>You have {messages.length} unread {messages.length > 1 ? "messages":"message"}</h1>
+        }
+
+        {/* What is "conditional rendering"?
+        When i want to only somtimes display something on the page base on a condition of some sort 
+        
+        when would you use &&?
+        When i want to either display something or not display it
+
+        when would you use a ternay?
+        when i need to decide wich thing among 2 options to display 
+
+        what if nedd to decide between > 2 options on what to display?
+        not recommeded using nested ternary, instead use and if else contioninal or a switch statement
+        */}
     </div>
   )
 }
